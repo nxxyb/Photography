4f4fe8a8433063eed491d6b5881dfbda1090b0c6
nxxyb <nxxyb@163.com>
2015-03-19 16:59:54 +0800

    company

--- app/com/photography/controller/ProjectController.java
+++ app/com/photography/controller/ProjectController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.photography.mapping.Project;
 import com.photography.service.IProjectService;
 
 /**
@@ -27,17 +30,32 @@ public class ProjectController extends BaseController {
 	public void setProjectService(IProjectService projectService) {
 		this.projectService = projectService;
 	}
+	
+	/**
+	 * 进入新建页面
+	 * @param request
+	 * @param model
+	 * @return
+	 * @author 徐雁斌
+	 */
+	@RequestMapping(value="/toCreate")
+	public ModelAndView toCreate(HttpServletRequest request, Model model){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("project/project_info");
+		return mav;
+	}
 
 	/**
-	 * 
-	 * @param user
+	 * 新建活动
+	 * @param project
+	 * @param imgFiles
 	 * @param request
 	 * @param model
 	 * @return
 	 * @author 徐雁斌
 	 */
 	@RequestMapping(value="/create")
-	public ModelAndView create(HttpServletRequest request, Model model){
+	public ModelAndView create(Project project,@RequestParam MultipartFile[] imgFiles,HttpServletRequest request, Model model){
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("project/project_info");
 		return mav;
