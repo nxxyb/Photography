3f4031e0a9d9ab4f86b6931ac618b333b7fef7a6
nxxyb <nxxyb@163.com>
2015-10-07 21:40:50 +0800

    home

--- app/com/photography/controller/WorkController.java
+++ app/com/photography/controller/WorkController.java
@@ -5,9 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.photography.mapping.Work;
 import com.photography.service.IWorkService;
 
 /**
@@ -52,6 +56,38 @@ public class WorkController extends BaseController {
 	@RequestMapping("/loadmore")
 	public String loadMore(String errorMessage,HttpServletRequest request, RedirectAttributes attr) {
 		return "work/work_loadmore";
+	}
+	
+	/**
+	 * 进入详细页面
+	 * @param request
+	 * @param model
+	 * @return
+	 * @author 徐雁斌
+	 */
+	@RequestMapping(value="/toReview")
+	public String toReview(String id,HttpServletRequest request, RedirectAttributes attr){
+//		ModelAndView mav = new ModelAndView();
+//		return reviewProject(id, request, mav);
+		return "work/work_review";
+	}
+	
+	/**
+	 * 进入新建页面
+	 * @param request
+	 * @param model
+	 * @return
+	 * @author 徐雁斌
+	 */
+	@RequestMapping(value="/toCreate")
+	public ModelAndView toCreate(String id,HttpServletRequest request, Model model){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("work/work_create");
+		
+		if(!StringUtils.isEmpty(id)){
+			mav.addObject("work", workService.loadPojo(Work.class,id));
+		}
+		return mav;
 	}
 
 }
